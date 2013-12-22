9416b3204a1ea8057bb0dd600f687a52e90ce1f7
Chris Veenboer <chris.veenboer@gmail.com>
2013-12-22 12:23:13 +0100

    mergesort.c now uses utils

--- mergesort.c
+++ mergesort.c
@@ -9,19 +9,9 @@
 
 #include "mach/mach_time.h"
 
+#include "utils.h"
+
 typedef void (*sorter)(int* numbers, int start, int end);
-
-void is_sorted(int* numbers, int from, int to) {
-
-	for( int i=from; i<to-1; i++ ) {
-		if( numbers[i] > numbers[i+1] ) {
-			printf("Fail at %d/%d (%d/%d)\n", i, i+1, numbers[i], numbers[i+1]);
-			exit(0);
-		}
-	}
-
-}
-
 
 
 void merge_sort(int** list, unsigned int count, unsigned int inner_sort_width, sorter inner_sorter) {
@@ -136,119 +126,6 @@ void swap2( int* numbers, int start, int end ) {
 	printf("Done  sort [%d,%d] = (%d,%d)\n", start, end, numbers[start], numbers[end]);
 }
 
-const int gaps[8] = {701, 301, 132, 57, 23, 10, 4, 1};
-
-// Sort an array a[start...end].
-// end is inclusive
-void shellsort_marcin( int* numbers, int start, int end ) {
-
-//	printf("shellsort [%d,%d]\n", start, end);
-	for(int s=start; s<=end; s++){
-//		printf( "\tn[%02d] = %d\n", s, numbers[s] );
-	}
-	int i, j, g;
-
-		// Do an insertion sort for each gap size.
-	for( g=0; g<8; g++ ) {
-		int gap = gaps[g];
-//		printf("\tGapsize %d\n", gap);
-		// i iterates over a virtual array defined by gapsize
-		// so if gap=3 then i iterates n[3], n[6], n[9] (starting at element 2)
-		if( start+gap <= end ) {
-//			printf("\tStarting at n[%02d] = %d\n", start+gap, numbers[start+gap]);
-		}
-		for (i = start+gap; i <= end; i += gap) { 
-		    int number_to_place = numbers[i];
-//			printf("\tcurrent number to place n[%02d] = %d\n",i, number_to_place);
-			// now iterate over every "gapth" element back to the left, moving items until
-			// we find one that is lower/equal than number_to_place
-			// For the first iteration, either the second number is larger and we're done, or move it to the position of the first element
-			// then we try element 3, move that left to its spot (since 1 and 2 are now sorted)
-//			printf("\tnow iterate from %d to %d\n", i-gap, start);
-		    for (j = i-gap; j >= start && numbers[j] > number_to_place; j -= gap) {
-//				printf("\tSince n[%02d] > n[%02d] (%d > %d = %d), moving n[%02d] right\n", j, i, numbers[j], number_to_place, numbers[j] > number_to_place, j );
-		        numbers[j+gap] = numbers[j];
-		    }
-//			printf("\tHole left at n[%02d], putting %d there\n", j+gap, number_to_place);
-		    numbers[j+gap] = number_to_place;
-		}
-		
-	}
-//	printf("\tss result [%d,%d]\n", start, end);
-	for(int s=start; s<=end; s++){
-//		printf( "\tn[%02d] = %d\n", s, numbers[s] );
-	}
-
-	
-}
-
-// read all numbers (space separated)
-// (random.org output)
-size_t read_numbers( const char* filename, int** numbers ) {
-	
-	FILE* in = fopen( filename, "rb" );
-	if( in == NULL ) {
-		perror("open()");
-		exit(0);
-	}
-	
-	// find out how many 4 byte ints
-	struct stat st;
-	int result = fstat( fileno(in), &st );
-	if( result == -1 ) {
-		perror("fstat()");
-		fclose( in );
-		exit(0);
-	}
-	size_t count = st.st_size / sizeof(int);
-	printf("Number of integers of size %ld bytes in file: %ld\n", sizeof(int), count);
-
-/*	
-	// we don't know how many, so start with space for 16, then realloc and double
-	int current_size = 256;
-	int* target = malloc( sizeof(int)*current_size );
-	if( target == NULL ) {
-		perror("malloc()");
-		exit(0);
-	}
-	printf("Starting target at %p\n", target);
-
-	int buf[256];
-	int read = 0;
-	int count = 0;
-	while( ( read = fread( buf, sizeof(int), 256, in ) ) > 0 ) {
-		printf("Read %d bytes, copying to %p\n", read, &target[count]);
-		memcpy( &target[count], buf, read*sizeof(int) );
-		count += read;
-		if( count >= current_size ) {
-			int* temp = realloc( target, sizeof(int)*current_size*2 );
-			if( temp == NULL ) {
-				perror("realloc()");
-				free(target);
-				exit(0);
-			}
-			target = temp;
-			printf("Realloc'd target to %p\n", target);
-		}
-	}
-	*/
-	
-	int* target = malloc( sizeof(int)*count );
-	if( target ==  NULL ) {
-		perror("malloc()");
-		exit( EXIT_FAILURE );
-	}
-	size_t read = fread( target, sizeof(int), count, in );
-	if( read != count ) {
-		printf("Read %ld ints, expected %ld\n", read, count);
-		free( target );
-		exit( EXIT_FAILURE );
-	}	
-	fclose( in );
-	*numbers = target;
-	
-	return count;
-}
 
 int compare_int(const void* a, const void* b) {
 
@@ -263,13 +140,14 @@ int compare_int(const void* a, const void* b) {
 
 int main(int argc, char *argv[]) {  
 	
-	if( argc != 3 ) {
-		puts("Usage: mergesort infile outfile");
+	if( argc != 4 ) {
+		puts("Usage: mergesort block_width infile outfile");
 		exit(0);
 	}
 	
-	const char* filename_in = argv[1];
-	const char* filename_out = argv[2];
+	const int block_width = atoi( argv[1] );
+	const char* filename_in = argv[2];
+	const char* filename_out = argv[3];
 	printf("Sorting file %s, writing to %s\n", filename_in, filename_out);
 	
 	int* numbers = NULL;
@@ -282,7 +160,7 @@ int main(int argc, char *argv[]) {
 	unsigned long start, stop;
 	start = mach_absolute_time();
 
-		merge_sort( &numbers, count, 701, shellsort_marcin );
+	merge_sort( &numbers, count, block_width, shellsort );
 
 // use builtin sort
 	//	mergesort( numbers, count, sizeof(int), compare_int);
