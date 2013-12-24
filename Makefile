87bc63349c0e9a66368e4e11b18b71d1da8504a1
Chris Veenboer <chris.veenboer@gmail.com>
2013-12-24 20:37:17 +0100

    Did the obvious thing and added shellsort

--- Makefile
+++ Makefile
@@ -3,20 +3,24 @@ CFLAGS=-Wall -O3 -pedantic
 CMD=${CC} ${CFLAGS}
 
 clean:
-	rm -f mergesort gen_random_ints pyramid_merge utils.o
+	rm -f bin/mergesort gen_random_ints bin/pyramid_merge bin/shellsort utils.o
 
-all: mergesort gen_random_ints pyramid_merge
+all: mergesort gen_random_ints pyramid_merge shellsort
 
 verbose: mergesort.c pyramid_merge.c utils.c
 	${CMD} -DVERBOSE -c utils.c
-	${CMD} -DVERBOSE utils.o mergesort.c -o mergesort
-	${CMD} -DVERBOSE utils.o pyramid_merge.c -o pyramid_merge
+	${CMD} -DVERBOSE utils.o mergesort.c -o bin/mergesort
+	${CMD} -DVERBOSE utils.o pyramid_merge.c -o bin/pyramid_merge
+	${CMD} -DVERBOSE utils.o shellsort.c -o bin/shellsort
 
 mergesort: mergesort.c utils.o
-	${CMD} utils.o mergesort.c -o mergesort
+	${CMD} utils.o mergesort.c -o bin/mergesort
 
 pyramid_merge: pyramid_merge.c utils.o
-	${CMD} utils.o pyramid_merge.c -o pyramid_merge
+	${CMD} utils.o pyramid_merge.c -o bin/pyramid_merge
+
+shellsort: shellsort.c utils.o
+	${CMD} utils.o shellsort.c -o bin/shellsort
 
 utils.o: utils.h utils.c
 	${CMD} -c utils.c
---
+++ shellsort.c
@@ -0,0 +1,54 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "time.h"
+#include "unistd.h"
+#include "sys/stat.h"
+
+#include "mach/mach_time.h"
+
+#include "utils.h"
+
+int main( int argc, char* argv[] ) {
+
+	if( argc != 3 ) {
+		puts("Usage: shellsort infile outfile");
+		exit(0);
+	}
+	
+	const char* filename_in = argv[1];
+	const char* filename_out = argv[2];
+	say("Sorting file %s, writing to %s\n", filename_in, filename_out);
+	
+	int* numbers = NULL;
+	size_t count = read_numbers( filename_in, &numbers );
+	say( "Read %zu numbers\n", count);
+	for(size_t i=0; i<count; i++ ) {
+		say( "N[%02zu] = %d\n", i, numbers[i] );
+	}
+	say("numbers premerge %p\n", numbers);
+
+	unsigned long start, stop;
+	start = mach_absolute_time();
+
+	shellsort( numbers, 0, count );
+
+	stop = mach_absolute_time();
+
+	mach_timebase_info_data_t timebase_info;
+	mach_timebase_info( &timebase_info );
+	
+	unsigned long elapsed_nano = (stop-start) * timebase_info.numer / timebase_info.denom;
+
+	// write count,nano
+	fprintf(stderr, "%ld,%ld\n", count,elapsed_nano);
+
+	say( "numbers postmerge %p\n", numbers );
+	print_array( numbers, 0, count, 8 );
+
+	is_sorted( numbers, 0, count );
+	
+	write_numbers( numbers, count, filename_out );
+	
+	free( numbers );
+	return 0;
+}
