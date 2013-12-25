188f75c4af7c6043287f1294ce02e726c0cd993e
Chris Veenboer <chris.veenboer@gmail.com>
2013-12-25 21:21:19 +0100

    insertionsort now has the same function signature as stdlib stuff, though I am sure the code I wrote will make real C programmers weep. There must be a better way and as soon as I read the libc source I am sure I will find out what it is :)

--- insertionsort.c
+++ insertionsort.c
@@ -1,5 +1,6 @@
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 #include "time.h"
 #include "unistd.h"
 #include "sys/stat.h"
@@ -8,22 +9,24 @@
 
 #include "utils.h"
 
-void insertionsort( int* n, size_t count, comparator compare ) {
+void insertionsort( void* base, size_t nel, size_t width, comparator compare ) {
 	
-	int value, hole_index;
-	for(int i=0; i<count; i++ ) {
-		value = n[i];
+	char* list = (char*)base;
+	void* value = malloc( width );
+	int hole_index;
+	for(int i=0; i<nel*width; i+=width ) {
+		memcpy( value, list+i, width ); // take value and keep it safe
 		hole_index = i; // this is where we took the value from, it's vacant
 		
-		say( "Moving value %d and creating a hole at %d\n", value, hole_index );
+		say( "Moving value %d and creating a hole at %d\n", *(int*)value, hole_index );
 		
-		while( hole_index > 0 && compare( &n[hole_index-1], &value ) == 1 ) { // if elements are higher, we shift them 
-			n[hole_index] = n[hole_index-1]; // move the element right
-			hole_index--; // move the hole left
+		while( hole_index > 0 && compare( list+hole_index-width, value ) == 1 ) { // if elements are higher, we shift them 
+			memcpy( list+hole_index, list+hole_index-width, width ); // move the element right
+			hole_index -= width; // move the hole left
 		}
-		n[hole_index] = value;
+		memcpy( list+hole_index, value, width );
 		
-		print_array( n, 0, count, 8 );
+		print_array( (int*)base, 0, nel, 8 );
 	}
 }
 
@@ -48,7 +51,7 @@ int main( int argc, char* argv[] ) {
 	unsigned long start, stop;
 	start = mach_absolute_time();
 
-	insertionsort( numbers, count, compare_int );
+	insertionsort( numbers, count, sizeof(int), compare_int );
 
 	stop = mach_absolute_time();
 
