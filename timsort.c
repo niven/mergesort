dad6c007585c47d2fe1ee675205bae57b283bdfb
Chris Veenboer <chris.veenboer@gmail.com>
2014-01-06 22:25:20 +0100

    Added calc_minrun() and fin_run() and reversing of descending runs

--- timsort.c
+++ timsort.c
@@ -1,14 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "utils.h"
 
 #define MIN(a,b) ( ((a)<(b)) ? (a) : (b) )
+
+int calc_minrun(size_t nel) {
+
+	int r = 0;  /* becomes 1 if the least significant bits contain at least one off bit */
+
+	while (nel >= 64) {
+		r |= nel & 1;
+	    nel >>= 1;
+	}
+
+	return nel + r;
+}
+
+ssize_t find_run( char* base, size_t nel, size_t width, comparator compare ) {
+	
+	if( nel < 2 ) {
+		return nel;
+	}
+	
+	size_t run_length = 2;
+	int asc = 1; 
+	if( compare( base, base+width ) > 0 ) { // base[0] > base[1]
+		asc = 0;
+	}
+	base += width;
+	
+	// too tired to figure out the elegant way to compa() >= asc or something.
+	int order = compare( base, base+width );
+	while( run_length <= nel && ( (asc && order <= 0) || (!asc && order > 0 ) ) ) {
+		run_length++;
+
+		base += width;
+		order = compare( base, base+width );
+	}
+	
+	return asc ? run_length : -run_length;
+}
 
 /*
 http://bugs.python.org/file4451/timsort.txt
 */
 void timsort(void* base, size_t nel, size_t width, comparator compare) {
 	
+	void* value = malloc( width );
+	if( value == NULL ) {
+		perror("malloc()");
+		exit( EXIT_FAILURE );
+	}
+	
+	int minrun = calc_minrun( nel );
+	say("Minrun for %zu elements: %d\n", nel, minrun);
+	
+	char* current = (char*)base;
+	size_t reached = 0;
+	
+	while( reached < nel ) {
+		
+		say("Current index %zu\n", reached);
+		
+		current = (char*)base + reached*width;
+		ssize_t run_length = find_run( current, nel-reached, width, compare );
+		say("Run length %zd\n", run_length);
+		print_array( (widget*)current, 0, abs(run_length), minrun );
+		if( run_length < 0 ) { // was descending
+			// reverse, though I'm sure there is a faster way.
+			char* left = current;
+			char* right = current + (abs(run_length)-1)*width;
+			say("Reversing from %d to %d\n", *(int*)left, *(int*)right);
+			while( left < right ) {
+				memcpy( value, left, width );
+				memcpy( left, right, width );
+				memcpy( right, value, width );
+				left += width;
+				right -= width;
+			}
+			say("Reversed descending run\n");
+			print_array( (widget*)current, 0, abs(run_length), minrun );
+		}
+		run_length = abs(run_length);
+		
+		// extend to minrun if needed
+		
+		// insertion sort
+		
+		reached += run_length;
+		
+	}
+	
+	// cheat
+	shellsort(base, nel, width, compare);
 
+	free( value );
 }
 
 
