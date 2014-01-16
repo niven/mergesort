57998a41b930d13c6f0e1a703075e8ff589626d8
Chris Veenboer <chris.veenboer@gmail.com>
2014-01-16 19:21:47 +0100

    It turns out the same bug was not present in merge_hi(), but it was different nonetheless.
    In my efforts to fix merging I assumed there was a problem with the bounding, while it was actually
    the find_index() function. This is how this bug got introduced: it solved another bug somewhere else,
    and I had not updated merge_hi() with the same bug, but instead fixed find_index(), revealing this one.
    
    The original code was correct all along, and also much nicer.
    
    Bounding the array by the insert index means just subtracting that from the count,
    which reduces to 0 when the insert location is after the array, since then count and index
    are equal.

--- timsort.c
+++ timsort.c
@@ -282,7 +282,7 @@ void merge_lo( run* a, run* b, size_t width, comparator compare ) {
 	RETURN_STDOUT
 	say("A[%d] (%d) should be placed at index %d in B (B[%d] = %d (could be out of bounds))\n", a->nel-1, *(int*) ( (char*) a->address + (a->nel-1)*width), last_a_in_b, last_a_in_b, *(int*) ( (char*)b->address + last_a_in_b*width) );
 	// this basically means B[last_a_in_b]-B[-1] are already sorted so we adjust B
-	b->nel -= b->nel - (MIN(last_a_in_b,b->nel-1)+1); // +1 since last_a_in_b is an index, while b->nel is a count. Nasty MIN there since last_a could come after the last valid index in B
+	b->nel -= b->nel - last_a_in_b;
 	say("B still starts with %d with %d elements:\n", *(int*)b->address, b->nel);
 	print_array( (widget*)b->address, 0, b->nel, b->nel);
 
@@ -398,6 +398,7 @@ void merge_hi( run* a, run* b, size_t width, comparator compare ) {
 	RETURN_STDOUT
 	say("A[%d] (%d) should be placed at index %d in B (B[%d] = %d (could be out of bounds))\n", a->nel-1, *(int*) ( (char*) a->address + (a->nel-1)*width), last_a_in_b, last_a_in_b, *(int*) ( (char*)b->address + last_a_in_b*width) );
 	// this basically means B[last_a_in_b]-B[-1] are already sorted so we adjust B
+	
 	b->nel -= b->nel - last_a_in_b;
 	say("B still starts at %d with %d elements:\n", *(int*)b->address, b->nel);
 	print_array( (widget*)b->address, 0, b->nel, b->nel);
